e3567119ee7c9c268c1426cbc409af8809280535
Samuel <KremplS21084@sus.htl-grieskirchen.at>
2024-09-29 18:13:51 +0200

    numberTester Methode done & Lambda also done

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,6 +4,42 @@ import java.util.ArrayList;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        NumberTester numberTester = new NumberTester("jjj");
+        NumberTest oddTester = number -> {
+            if(number % 2 == 0){
+                return false;
+            }
+            return true;
+        };
 
+        NumberTest primeTester = number -> {
+            if(number == 1 || number == 0){
+                return true;
+            }
+            for(int i = 2;i < 10;i++){
+                if(number%i == 0){
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        NumberTest palindromTester = number -> {
+            String temp = ""+number;
+            char[] c = temp.toCharArray();
+            char[] returnChar = c;
+
+            int x = c.length;
+            for(int i = 0;i <= c.length;i++){
+               if(returnChar[i] != c[x]){
+                   return false;
+               }
+            }
+            return true;
+        };
+
+        numberTester.setOddEventTester(oddTester);
+        numberTester.setPrimeTester(primeTester);
+        numberTester.setPalindromTester(palindromTester);
     }
 }
--- untitled/src/NumberTester.java
+++ untitled/src/NumberTester.java
@@ -1,21 +1,56 @@
-public class NumberTester {
-    NumberTester oddTester;
-    NumberTester primeTester;
-    NumberTester palindromTester;
+import java.io.*;
 
-    public void numberTester(String fileName){
+public class NumberTester{
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromTester;
 
+    private int numberOfTests;
+
+    private int[] numbers;
+    private String path;
+
+    public NumberTester(String path) {
+        this.path = path;
     }
 
-    public void setOddEventTester(NumberTester oddTester){
+    public void numberTester(String fileName) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(fileName));
+            String line = bf.readLine();
+            numberOfTests = Integer.parseInt(line);
+            for (int i = 0; i <= numberOfTests; i++) {
+                line = bf.readLine();
+                String[] temp = line.split(" ");
+
+                int testingNumber = Integer.parseInt(temp[1]);
+                switch (Integer.parseInt(temp[0])){
+                    case 1: oddTester.testNumber(testingNumber);break;
+                    case 2: primeTester.testNumber(testingNumber);break;
+                    case 3: palindromTester.testNumber(testingNumber);break;
+                }
+
+            }
+        } catch (
+                FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setOddEventTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
 
-    public void setPrimeTester(NumberTester primeTester){
+    public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
     }
 
-    public void setPalindromTester(NumberTester palindromTester) {
+    public void setPalindromTester(NumberTest palindromTester) {
         this.palindromTester = palindromTester;
     }
+
+
 }
